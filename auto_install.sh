32ef80b9daf4de022500fdcc92b10664427f12c4
Christian Medel <cmedelahumada@gmail.com>
2018-10-12 07:59:35 -0400

    some corrections after testing

--- auto_install.sh
+++ auto_install.sh
@@ -8,13 +8,21 @@ mkdir windows-10
 cd windows-10
 
 # get source
-wget https://github.com/B00merang-Project/Windows-10
+wget https://github.com/B00merang-Project/Windows-10/archive/master.zip -q --show-progress
 
-# extract
-unzip master.zip
+# extract without verbose
+unzip -qq master.zip
+
+# create .themes if not exist
+if [ ! -d "$HOME/.themes" ]
+then
+  mkdir "$HOME/.themes"
+fi
 
 # rename and move to $HOME/.themes
-mv Windows-10-master Windows\ 10
-mv Windows\ 10 ~/.themes
+mv Windows-10-master "$HOME/.themes/Windows 10"
+
+# delete temporary directory
+rm -rf /tmp/windows-10
 
 echo "Install finished. Check your distro's customization tool to apply changes"
