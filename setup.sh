aa881a9926a1fbff5ab8340ed672609f1a6134dc
Christian Medel <cmedelahumada@gmail.com>
2018-10-17 16:33:13 -0400

    Correct ATP command

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 killall update-manager
-sudo apt uninstall update-manager
+sudo apt remove update-manager
 sudo apt-add-repository ppa:flexiondotorg/awf
 sudo apt update
 sudo apt install -y awf gtk-3-examples gnome-tweaks git gcc make perl gnome-shell-extensions
