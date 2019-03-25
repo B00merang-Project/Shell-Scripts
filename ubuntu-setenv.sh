880da7194e9023eeb55c5b0dfa8a06eb94d6da93
Christian Medel <elbullazul@localhost.localdomain>
2019-03-25 14:16:47 -0400

    Add description

--- ubuntu-setenv.sh
+++ ubuntu-setenv.sh
@@ -4,5 +4,6 @@ killall update-manager
 sudo apt remove update-manager
 sudo apt-add-repository ppa:flexiondotorg/awf
 sudo apt update
+# gcc make perl are for VirtualBox guest additions
 sudo apt install -y awf gtk-3-examples gnome-tweaks git gcc make perl gnome-shell-extensions
 sudo apt upgrade -y
