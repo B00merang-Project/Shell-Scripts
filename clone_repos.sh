fb55ddb05fe16a56a44444fe865f5cb778be4666
Christian Medel <cmedelahumada@gmail.com>
2019-06-09 14:06:25 -0400

    Update linker & pull URL

--- clone_repos.sh
+++ clone_repos.sh
@@ -4,7 +4,7 @@ read -p "Repos will be cloned here. Continue? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  wget https://gist.githubusercontent.com/Elbullazul/e5182157a7b8762a2941cd52ee6f7f25/raw/6b0825cb200f4b2af760f94fa67d0c54120bda1e/themes.index
+  wget https://gist.githubusercontent.com/Elbullazul/e5182157a7b8762a2941cd52ee6f7f25/raw/52d4407535cc44a8c014ac197547af7355553139/themes.index
 
   while read p; do
   git clone "$p"
