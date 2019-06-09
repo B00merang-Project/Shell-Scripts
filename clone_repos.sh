4744c5007d8391fa99ea6b602a4625bc59c2d5fc
Christian Medel <cmedelahumada@gmail.com>
2019-06-09 14:11:30 -0400

    Update scripts

--- clone_repos.sh
+++ clone_repos.sh
@@ -4,13 +4,13 @@ read -p "Repos will be cloned here. Continue? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  wget https://gist.githubusercontent.com/Elbullazul/e5182157a7b8762a2941cd52ee6f7f25/raw/52d4407535cc44a8c014ac197547af7355553139/themes.index
+  wget https://gist.githubusercontent.com/Elbullazul/e5182157a7b8762a2941cd52ee6f7f25/raw/themes.index
+  
+  while IFS= read -r url || [ -n "$url" ]; do
+    git clone "$url"
+  done < themes.index
 
-  while read p; do
-  git clone "$p"
-done <themes.index
-
-rm themes.index
+  rm themes.index
 else
   echo "Aborted."
 fi
