d111b8773256385726a6288c782cb7e7c4eb53ed
Christian Medel <cmedelahumada@gmail.com>
2019-07-11 08:41:33 -0400

    Add sass

--- ubuntu-setenv.sh
+++ ubuntu-setenv.sh
@@ -5,5 +5,5 @@ sudo apt remove update-manager gnome-shell-extension-desktop-icons gnome-shell-e
 sudo apt-add-repository ppa:flexiondotorg/awf
 sudo apt update
 # gcc make perl are for VirtualBox guest additions
-sudo apt install -y awf gtk-3-examples gnome-tweaks git gcc make perl gnome-shell-extensions
+sudo apt install -y awf gtk-3-examples gnome-tweaks git gcc make perl gnome-shell-extensions sass
 sudo apt upgrade -y
