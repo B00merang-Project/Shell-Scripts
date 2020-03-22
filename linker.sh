90b72623011fd13ed26c69cd649f3641befa76dc
Christian Medel <cmedelahumada@gmail.com>
2020-03-22 08:41:36 -0700

    Adapt to 20.04

--- linker.sh
+++ linker.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+if [ ! -d $HOME/.themes ]; then
+    mkdir $HOME/.themes
+fi
+
 for D in *; do
-    if [ -d "${D}" ] && [ -d "${D}/gtk-3.0" ]; then
-        ln -s "$PWD/${D}" "$HOME/.themes"  # your processing here
+    if [ -d "${D}" ]; then
+        if [ -d "${D}/gtk-3.0" ]; then
+            echo "Linking ${D}"
+            ln -s "$PWD/${D}" "$HOME/.themes"
+        else
+            cd "${D}"
+            for SD in *; do
+                if [ -d "${SD}" ] && [ -d "${SD}/gtk-3.0" ]; then
+                    echo "Linking ${SD}"
+                    ln -s "$PWD/${SD}" "$HOME/.themes"
+                fi
+            done
+            cd ..
+        fi
     fi
 done
--- ubuntu-setenv.sh
+++ ubuntu-setenv.sh
@@ -2,8 +2,7 @@
 
 killall update-manager
 sudo apt remove update-manager gnome-shell-extension-desktop-icons gnome-shell-extension-ubuntu-dock/
-sudo apt-add-repository ppa:flexiondotorg/awf
 sudo apt update
 # gcc make perl are for VirtualBox guest additions
-sudo apt install -y awf gtk-3-examples gnome-tweaks git gcc make perl gnome-shell-extensions sass
+sudo apt install -y gtk-3-examples gnome-tweaks git gcc make perl gnome-shell-extensions sass
 sudo apt upgrade -y
