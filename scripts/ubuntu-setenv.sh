b0e3de68beea71ac0dcfd49432df689f3c9f50ba
Christian Medel <cmedelahumada@gmail.com>
2021-11-23 11:20:58 -0500

    Cleanup

--- scripts/ubuntu-setenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall update-manager
-sudo apt remove update-manager gnome-shell-extension-desktop-icons gnome-shell-extension-ubuntu-dock/
-sudo apt update
-# gcc make perl are for VirtualBox guest additions
-sudo apt install -y gtk-3-examples gnome-tweaks git gcc make perl gnome-shell-extensions sass
-sudo apt upgrade -y
