e6b445e1993a0d3f959670db58072e70f4e575be
Christian Medel <cmedelahumada@gmail.com>
2022-09-15 12:55:36 -0400

    Add installation script for GTK 4 themes

---
+++ gtk4-installer.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# greet the user!
+echo "  _        ___     ___                                                    
+ | |__    / _ \   / _ \   _ __ ___     ___   _ __    __ _   _ __     __ _ 
+ | '_ \  | | | | | | | | | '_ \` _ \   / _ \ | '__|  / _\` | | '_ \   / _\` |
+ | |_) | | |_| | | |_| | | | | | | | |  __/ | |    | (_| | | | | | | (_| |
+ |_.__/   \___/   \___/  |_| |_| |_|  \___| |_|     \__,_| |_| |_|  \__, |
+                                                                    |___/ "
+
+echo "Welcome to b00merang's GTK 4 theme installer"
+read -n 1 -s -r -p "Press any key to continue..."
+
+# declare variables
+declare -a USER_THEMES
+declare -a SHARED_THEMES
+
+# check if gtk-4.0 folder present
+[ ! -d "$HOME/.config/gtk-4.0" ] && printf "\ngtk-4.0 folder not found, you probably are not on using gtk-4.0\n" && exit
+
+# TODO: override with `/usr/share/themes` if flag received
+theme_folder="$HOME/.themes"
+
+# find theme folders found in ~/.themes with a `gtk-4.0` folder and `gtk-4.0/gtk.css` file
+if [ -d $theme_folder ]; then
+  for theme in "$theme_folder/"*; do
+    [ ! -d "$theme/gtk-4.0" ] && continue
+    [ ! -f "$theme/gtk-4.0/gtk.css" ] && continue
+    
+    # store paths in array 
+    USER_THEMES+=("$theme")
+  done
+else
+  echo "$theme_folder does not exist, try again"
+  exit
+fi
+
+# erase press to continue line
+printf "\rList of available themes:"
+echo
+
+# show themes in 2 columns with a number
+pr -2T <<< $(for (( t=1; t<=${#USER_THEMES[@]}; t++ )); do
+  echo "  $t `basename ${USER_THEMES[$(($t - 1))]}`"
+done)
+
+input=""
+theme_count=${#USER_THEMES[@]}
+
+# ask for user choice and check if answer is valid
+while true; do
+  read -p "Enter number of theme to install (1-$theme_count) " input
+  if [ $input -gt 0 ] && [ $input -le $theme_count ]; then
+    break;
+  fi
+  
+  # if answer not correct, keep asking until it is, or user closes script
+  echo "Invalid number: $input"
+done
+
+selected_theme=${USER_THEMES[$input - 1]}
+
+# if another theme is already present in ~/gtk-4.0, ask for confirmation
+if [ -f "$HOME/.config/gtk-4.0/gtk.css" ]; then
+  read -p "A theme is already installed, do you want to overwrite it? (y/N) " yesno
+  
+  [[ ! $yesno =~ ^[Yy]$ ]] && echo "Operation was cancelled" && exit
+fi
+
+# copy everything in $selected_theme/gtk-4.0 to ~/.config/gtk-4.0
+echo "Copying theme '`basename $selected_theme`'"
+
+cp -R "$selected_theme/gtk-4.0/"* "$HOME/.config/gtk-4.0"
+
+echo "Operation complete"
