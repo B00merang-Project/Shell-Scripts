67983c32743d3504c98389ad7ba74ce87796ed0d
Christian Medel <dev@elbullazul.net>
2024-07-25 18:14:04 -0400

    Update index URL

--- clone-repos.sh
+++ clone-repos.sh
@@ -4,7 +4,7 @@ read -p "Repos will be cloned here. Continue? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  wget https://gist.githubusercontent.com/Elbullazul/e5182157a7b8762a2941cd52ee6f7f25/raw/themes.index
+  wget https://gist.githubusercontent.com/Elbullazul/3c5e746f224d8abde5b0e6a6d4809200/raw/f4098fa755038379d7095d4bb631e29a9e5a72af/themes.index
   
   while IFS= read -r url || [ -n "$url" ]; do
     git clone "$url"
